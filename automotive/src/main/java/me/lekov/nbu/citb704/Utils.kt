a0f1469c25703d984fd4cc90308a77e79ad94b54
Asen Lekov <asenlekoff@gmail.com>
2022-01-05 23:19:15 +0200

    Update Utils.kt
    Gracefully handle streams and exceptions handling

--- automotive/src/main/java/me/lekov/nbu/citb704/Utils.kt
+++ automotive/src/main/java/me/lekov/nbu/citb704/Utils.kt
@@ -2,6 +2,7 @@ package me.lekov.nbu.citb704
 
 import org.json.JSONObject
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -9,18 +10,26 @@ import java.net.URL
 
 fun fetchJson(url: String): JSONObject? {
     val Url = URL(url)
-    val connection: HttpURLConnection = Url.openConnection() as HttpURLConnection
-    val inputStream: InputStream = connection.inputStream
-    val br = BufferedReader(InputStreamReader(inputStream))
-    val sb = StringBuilder()
-    var line: String?
-    while (br.readLine().also { line = it } != null) {
-        sb.append(line)
-    }
-    line = sb.toString()
-    connection.disconnect()
-    inputStream.close()
-    sb.delete(0, sb.length)
-    return JSONObject(line)
+
+    try {
+        val connection: HttpURLConnection = Url.openConnection() as HttpURLConnection
+        val inputStream: InputStream = connection.inputStream
+        val sb = StringBuilder()
+
+        inputStream.use {
+            val br = BufferedReader(InputStreamReader(inputStream))
+
+            br.useLines {
+                it.iterator().forEachRemaining {
+                    sb.append(it)
+                }
+            }
+        }
+
+        connection.disconnect()
+        return JSONObject(sb.toString())
+    } catch(e: Exception) {}
+
+    return null
 }
 
